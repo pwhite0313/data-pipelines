8284e13c04aaefd5423b975378d31c623a0df55a
Patrick <pwhite0313@gmail.com>
2026-03-19 16:07:42 -0400

    changed load .py file to allow DAG to pass in airflow filepath

--- src/load.py
+++ src/load.py
@@ -3,9 +3,9 @@ import pandas as pd
 from datetime import datetime
 from utils import RAW_DATA_DIR
 
-def load_records(records) -> str:
+def load_records(records, output_path) -> str:
     timestamp = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
-    output_path = RAW_DATA_DIR / f"records_{timestamp}.csv"
+    # output_path = RAW_DATA_DIR / f"records_{timestamp}.csv" # Commented out to use with DAG
 
     with open(output_path, "w", encoding="utf-8", newline="") as f:
         records.to_csv(f, index=False)
