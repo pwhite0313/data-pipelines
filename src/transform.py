55c91b6d8b572b1c42f391e2f2415d79efb2dede
Patrick <pwhite0313@gmail.com>
2026-04-10 20:37:04 -0400

    added validate function to isolate try except blocks in tranformation script. Encountered syntax error and DAG failed cleany. Traced the error via logs and resolved. DAG ran on next retry. Amazing.

--- src/transform.py
+++ src/transform.py
@@ -3,46 +3,52 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 
+def validate_response(data):
+
+    if not isinstance(data, dict):
+        raise ValueError("API response must be a dictionary")
+    
+    if "list" not in data:
+        raise ValueError("API response missing 'list' key")
+    
+    if not isinstance(data["list"], list):
+        raise ValueError("'list' must be a list inside the 'data' dictionary")
+    
+    if "city" in data and not isinstance(data["city"], dict):
+        raise ValueError("'city', was found but is not a dictionary")
+
 ## Function to call cities by name and return weather
 def transform_records(data):
 
     logger.info("Transformation started")
     
-    if not isinstance(data,dict) or "list" not in data:
-        logger.error("Invalid structure: missing expected 'list'")
     try:
-        # Normalize as Pandas DF
-        df = pd.json_normalize(data['list'], sep="_")
-    
-    except Exception:
-        logger.exception("Failed to normalize main data")
+        validate_response(data)
+    except:
+        logger.exception("Validation failed")
         raise
 
-    try:
-        df["weather"] = df["weather"].apply(
-            lambda x: x[0] if isinstance(x, list) and len(x) > 0 else {}
-        )
+    df = pd.json_normalize(data['list'], sep="_")
 
-        df_weather = pd.json_normalize(df["weather"]).add_prefix("weather_")
-        df = df.drop(columns=["weather"]).join(df_weather)
+    # Extract first weather object from list (API returns weather as a list of dicts)
+    # If missing or invalid, default to empty dict to avoid downstream errors
+    df["weather"] = df["weather"].apply(
+        lambda x: x[0] if isinstance(x, list) and len(x) > 0 else {}
+    )
 
-    except:
-        logger.exception("Failed to process data")
+    df_weather = pd.json_normalize(df["weather"]).add_prefix("weather_")
+    df = df.drop(columns=["weather"]).join(df_weather)
 
     # Convert column to date_time
-    df["dt_txt"] = pd.to_datetime(df["dt_txt"])
+    df["dt_txt"] = pd.to_datetime(df["dt_txt"], errors="coerce")
+    df = df.dropna(subset=["dt_txt"])
 
-    try:
-        city_data = data.get("city", {})
+    city_data = data.get("city", {})
 
-        if city_data:   
-            # Add flattened city keys
-            city_df = pd.json_normalize(data.get("city", {}), sep="_").add_prefix("city_")
-            df = df.assign(**city_df.iloc[0].to_dict())
-
-    except:
-        logger.warning("No city data found")
-        raise
+    if city_data:
+        # Add flattened city keys
+        city_df = pd.json_normalize(data.get("city", {}), sep="_").add_prefix("city_")
+        df = df.assign(**city_df.iloc[0].to_dict())
 
     logger.info("Transformed to Pandas DF of shape: %s", df.shape)
 
