3a775a8f73c4a38ff2c96a64bc1c3448e4752724
pwhite <pwhite0313@gmail.com>
2026-05-14 16:30:14 -0400

    feat: convert fct_weather_forecast to incremental model

--- dbt/models/marts/fct_weather_forecast.sql
+++ dbt/models/marts/fct_weather_forecast.sql
@@ -1,4 +1,6 @@
-select 
+{{ config(materialized='incremental', unique_key=['city_id', 'local_dt']) }}
+
+select
     local_dt,
     city_country,
     city_name,
@@ -8,11 +10,15 @@ select
     main_temp_max,
     main_feels_like,
     main_humidity,
-    clouds_all
+    clouds_all,
     rain_3h,
     wind_speed,
     wind_gust,
     weather_main,
     weather_description,
     weather_id
-FROM {{ ref('stg_weather__forecast') }}
+from {{ ref('stg_weather__forecast') }}
+
+{% if is_incremental() %}
+    where local_dt > (select max(local_dt) from {{ this }})
+{% endif %}
